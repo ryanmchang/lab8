b9cca3a8d7f0f5901f1966628b9bd7058239f8f8
ryanmchang <ryanmchang@gmail.com>
2020-02-27 00:10:08 -0800

    First commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,18 +10,46 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	//initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
-// init jQuery gestures  
+// init jQuery gestures
 function initGestures() {
-	// add gestures listener here
+	$(function() {
+    $(".judge-img").bind("taphold", tapholdHandler);
+    function tapholdHandler(event) {
+      var targetIDPrefix = event.target.id;
+      console.log("got prefix: " + targetIDPrefix);
+      $("#" + targetIDPrefix + "-bio").show();
+    }
+  });
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+  $('#rsvpForm').submit(function(e){
+    e.preventDefault();
+    console.log("submitting form...");
+    var rsvpEmail = $("#rsvpEmail").val();
+    $.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+  });
+  function postCallback(res) {
+    alert("RSVP form successfully submitted!");
+    $('#rsvpEmail').val('');
+  }
+}
+function initMap() {
+	// add your code here
+  L.mapquest.key = 't69jGscXR3NtI8ZLv05jXO6HWnQDVL7i';
+  var map = L.mapquest.map('map', {
+    center: [32.88, -117.236],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.88, -117.236]).addTo(map);
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = 't69jGscXR3NtI8ZLv05jXO6HWnQDVL7i';
+  var map = L.mapquest.map('map', {
+    center: [32.88, -117.236],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.88, -117.236]).addTo(map);
 }
